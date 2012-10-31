fcbdbf4c574b8f29c463168fb2c35262d197ae44
josue <josss9229@gmail.com>
2012-10-31 23:12:28 +0000

    Primeras creaciones

---
+++ lib/clase_fraccion.rb
@@ -0,0 +1,18 @@
+
+class Fraccion
+attr_accessor :num, :denom
+
+   def gcd(x,y)
+	y==0? x:  gcd(y,x%y) 
+   end
+   def lcm(x,y)
+    z=x/gcd(x,y) *y
+   end
+  def initialize(n,d)
+    raise TypeError, "El denominador no puede ser 0" if d.eql?0
+    m=gcd(n,d)
+    @num=n/m
+    @denom=d/m
+  end
+
+end
---
+++ spec/rspec_fraccion.rb
@@ -0,0 +1,24 @@
+require "~/LPP/Pract7/lib/clase_fraccion"
+
+ describe Fraccion do
+
+  before :each do
+
+    @obj_1=Fraccion.new(1,2)
+  end
+    it"Deberia haber un numerador y denominador" do
+      @obj_1.denom.should==2
+      @obj_1.num.should==1
+    end
+   it "Deberia ser minimo el numerador y denominador" do
+        p=@obj_1.num
+        q=@obj_1.denom
+        m=@obj_1.gcd(p,q)
+        @obj_1.denom.should == q/m
+        @obj_1.num.should == p/m
+  end
+
+end
+
+
+
